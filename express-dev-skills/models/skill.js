4167b1904e853491bbbe32b506ef32c74081eaeb
Sam Hay <snh68@yahoo.com>
2022-10-25 22:23:37 -0700

    add lab

---
+++ express-dev-skills/models/skill.js
@@ -0,0 +1,10 @@
+// models/skill.js
+const skills = ['Cooking', 'Coding', 'JavaScript', 'HTML', 'CSS'];
+
+module.exports = {
+    getAll,
+};
+
+function getAll() {
+    return skills;
+}
---
+++ express-dev-skills/routes/skills.js
@@ -0,0 +1,9 @@
+//routes/skills.js
+var express = require('express');
+var router = express.Router();
+
+var skillsCtrl = require('../controllers/skills')
+
+router.get('/', skillsCtrl.index)
+
+module.exports = router;
